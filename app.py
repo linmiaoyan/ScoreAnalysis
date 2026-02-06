0f867b3a3974b34d03ee43d58327e3cf95b4cfb0
linmy <657894692@qq.com>
2026-02-06 11:17:28 +0800

    snap

--- app.py
+++ app.py
@@ -86,9 +86,9 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    """主页"""
+    """主页（允许 POST 以免表单误提交时返回 405）"""
     return render_template('index.html')
 
 
